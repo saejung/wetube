d81cddfb8e66cf8eda2b1c3a0a7613c8dcecc111
saejung <39181415+saejung@users.noreply.github.com>
2019-01-21 18:00:33 +0900

    2019-01-21 18:00 commit

--- index.js
+++ index.js
@@ -6,4 +6,19 @@ function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+// function handleHome(){
+//    console.log("Hi. from home!!");
+// }
+
+function handleHome(req, res){
+//    console.log(req);
+   res.send("Hi. from home!!");
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile!!");
+}
+
+app.get("/profile", handleProfile)
+app.get("/", handleHome);
 app.listen(PORT, handleListening);
